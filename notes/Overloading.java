d4cc956be7c7652e0df6091eafad1ff99eacc69c
swasthiknrao <your-email@example.com>
2025-12-15 15:51:15 +0530

    Date 15-dec

--- notes/Overloading.java
+++ notes/Overloading.java
@@ -1,37 +1,35 @@
 //calculate the area of circle  square rectagle use methid overloading
 import java.util.Scanner;
 class AreaCalculator {
-    // Method to calculate area of a circle
+ 
     public double area(double radius) {
-        return Math.PI * radius * radius;
+        return 3.14 * radius * radius;
     }
 
-    // Method to calculate area of a square
     public double area(int side) {
         return side * side;
     }
 
-    // Method to calculate area of a rectangle
     public double area(int length, int width) {
         return length * width;
     }
 }
-public class Overload {
+public class Overloading {
     public static void main(String[] args) {
         AreaCalculator calculator = new AreaCalculator();
         Scanner sc = new Scanner(System.in);
 
-        // Calculate area of circle
+        
         System.out.print("Enter radius of the circle: ");
         double radius = sc.nextDouble();
         System.out.println("Area of Circle: " + calculator.area(radius));
 
-        // Calculate area of square
+ 
         System.out.print("Enter side of the square: ");
         int side = sc.nextInt();
         System.out.println("Area of Square: " + calculator.area(side));
 
-        // Calculate area of rectangle
+
         System.out.print("Enter length and width of the rectangle: ");
         int length = sc.nextInt();
         int width = sc.nextInt();
